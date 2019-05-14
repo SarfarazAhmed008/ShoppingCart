0d3b6444aef09394a0e71fe7fd9d959a02fb3715
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2019-05-14 14:38:55 +0600

    refactored code of file system

--- models/product.js
+++ models/product.js
@@ -2,6 +2,19 @@ const path = require('path');
 const fs = require('fs');
 
 //const products = [];
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'data-file.json');
+
+const getProductsFromFile = callback => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err){
+            console.log(err);
+            callback([]);
+        }
+        else{
+            callback(JSON.parse(fileContent));
+        }
+    });
+};
 
 module.exports = class Product {
     constructor(t) {
@@ -13,29 +26,25 @@ module.exports = class Product {
         //console.log('save method executed', this);
         
         //save data into the file as json format
-        let products = [];
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'data-file.json');
-        fs.readFile(p, (err, fileContent) => {
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        // let products = [];
+        //const p = path.join(path.dirname(process.mainModule.filename), 'data', 'data-file.json');
+        //fs.readFile(p, (err, fileContent) => {});
+
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
                 console.log('error is', err);
             });
         });
+        
+  
     }
 
     static fetchAll(callback){
-        //return products;
-        
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'data-file.json');
-        fs.readFile(p, (err, fileContent) => {
-            if(err){
-                console.log(err);
-                return callback([]);
-            }
-            callback(JSON.parse(fileContent));
-        });
+        // return products;
+        // const p = path.join(path.dirname(process.mainModule.filename), 'data', 'data-file.json');
+
+        getProductsFromFile(callback);
+   
     }
 };
