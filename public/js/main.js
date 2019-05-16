57a6a7a11ae6b8378593339cf3d30ec90fad3d21
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2019-05-16 12:07:32 +0600

    mob style added

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
-alert('Welcome to the shop');
+//alert('Welcome to the shop');
+
+
+const backdrop = document.querySelector('.backdrop');
+const sideDrawer = document.querySelector('.mobile-nav');
+const menuToggle = document.querySelector('#side-menu-toggle');
+
+function backdropClickHandler() {
+  backdrop.style.display = 'none';
+  sideDrawer.classList.remove('open');
+}
+
+function menuToggleClickHandler() {
+  backdrop.style.display = 'block';
+  sideDrawer.classList.add('open');
+}
+
+backdrop.addEventListener('click', backdropClickHandler);
+menuToggle.addEventListener('click', menuToggleClickHandler);
