a9381ece78b00c895bf886cc47670dbe16a96ed9
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2019-05-27 14:14:44 +0600

    db connection (sequelize)

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    database : 'node_complete',
-    password : 'password'
+const sequelize = new Sequelize('node_complete', 'root', 'password', {
+    dialect : 'mysql',
+    host : 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host : 'localhost',
+//     user : 'root',
+//     database : 'node_complete',
+//     password : 'password'
+// });
+
+// module.exports = pool.promise();
+
+
