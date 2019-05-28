8e206d3825f5d3c0b44e6387c03fd9e5f43743bd
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2019-05-28 15:11:01 +0600

    product model created

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+//const db = require('./util/database');
 
 const app = express();
 
@@ -37,4 +38,10 @@ app.use(shopRouter);
 
 app.use(errorController.get404Page);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        //console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
+
--- models/product.js
+++ models/product.js
@@ -1,31 +1,27 @@
-const Cart = require('./cart');
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-    constructor(id, title, imageUrl, price, description) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.description = description;
+const Product = sequelize.define('product', {
+    id : {
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        allowNull : false,
+        primaryKey : true
+    },
+    title : Sequelize.STRING,
+    price : {
+        type : Sequelize.DOUBLE,
+        allowNull : false
+    },
+    imageUrl : {
+        type : Sequelize.STRING,
+        allowNull : false
+    },
+    description : {
+        type : Sequelize.STRING,
+        allowNull : false
     }
+});
 
-    save() {
-        return db.execute('INSERT INTO products (title, imageUrl, price, description) VALUES(?, ?, ?, ?)', 
-            [this.title, this.imageUrl, this.price, this.description]);
-    }
-
-    static fetchAll(){
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id){
-        return db.execute('SELECT * FROM products WHERE products.id = ?', 
-        [id]);
-    }
-
-    static deleteById(id){
-        return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
-    }
-};
+module.exports = Product;
